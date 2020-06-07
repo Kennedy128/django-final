33a8c2d81a692c7841af2962d9fe6b966bf1c6d9
Santa claus <santa@northpole.com>
2020-06-08 01:11:00 +0300

    creating forms for our profile

---
+++ rate/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Profile,Project,Review
+
+class ProfileForm(forms.ModelForm):
+    '''
+    class to define profile form
+    '''
+    class Meta:
+        model = Profile
+        exlcude = ['user']
+        fields = ('bio', 'profile_pic','contacts')
+
+class ProjectForm(forms.ModelForm):
+    '''
+    class to define project form
+    '''
+    class Meta:
+        model = Project
+        exlcude = ['user']
+        fields = ('title','description','link','image') 
+
+class ReviewForm(forms.ModelForm):
+    '''
+    class to define Review form
+    '''
+    class Meta:
+        model = Review
+        exlcude = ['user','project','overall_score']
+        fields = ('comment','design','usability','content')    
